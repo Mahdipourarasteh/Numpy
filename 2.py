a248c55f2e90c39bcc72df5897afd0081a1955b7
Mahdipourarasteh <mahdi.mahdipourarasteh@gmail.com>
2022-09-14 22:54:30 +0200

    new commit

--- 2.py
+++ 2.py
@@ -3,9 +3,21 @@ a= np.arange(15)
 
 print(a)
 print(type(a))
-print(a.ndim)
-print(a.shape)
+print(a.ndim, a.shape, a.size, a.dtype.name, a.itemsize)
 
-print(a.size)
-print(a.dtype.name)
-print(a.itemsize)
+# print(a.shape)
+# print(a.size)
+# print(a.dtype.name)
+# print(a.itemsize)
+print('----------------------')
+
+c = np.array([[1,2], [4,5]])
+
+print(c)
+print(type(c))
+print(c.ndim, c.shape, c.size, c.dtype.name, c.itemsize)
+
+# print(c.shape)
+# print(c.size)
+# print(c.dtype.name)
+# print(c.itemsize)
